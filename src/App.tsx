3d13b8d7ac0035eddd7297ae7a6e009dc11f5d77
Enzo Coronel <enzocoronel1000@gmail.com>
2023-02-17 09:41:02 -0300

    UI template
    Finished the first overall display for future functions implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Display from "./components/Display/Display";
+import Pad_Btn from "./components/Pad_Btn/Pad_Btn";
+
+const buttons = [
+  "AC",
+  "()",
+  "%",
+  "÷",
+  "7",
+  "8",
+  "9",
+  "X",
+  "4",
+  "5",
+  "6",
+  "-",
+  "1",
+  "2",
+  "3",
+  "+",
+  "0",
+  ",",
+  "x",
+  "=",
+];
 
 function App() {
+  const [pressed, setPressed] = useState("");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header className="text-right mr-6">
+        <p>Options</p>
       </header>
+      <Display props={pressed} />
+      <div className="grid grid-cols-4 absolute w-screen h-1/2 bottom-0 border-t-2">
+        {buttons.map((element: string) => {
+          return <Pad_Btn props={element} setPressed={setPressed} />;
+        })}
+      </div>
     </div>
   );
 }
