07f6271e2f7eb12862a035f2519f133b438809d2
Enzo Coronel <enzocoronel1000@gmail.com>
2023-03-02 13:28:39 -0300

    equation processing finished
    Added handle MultiplicationAndDivisions function and a few bug fixes

--- src/components/PadBtn/PadBtn.tsx
+++ src/components/PadBtn/PadBtn.tsx
@@ -11,7 +11,7 @@ const PadBtn: React.FC<Props> = ({
 }) => {
   const updateCalculationState = () => {
     let newCalc = { ...calc };
-    let mainString = newCalc.displayedString;
+    let mainStr = newCalc.displayedString;
 
     if (input === "AC") {
       setCalc({
@@ -25,25 +25,27 @@ const PadBtn: React.FC<Props> = ({
     }
 
     if (input === "=") {
-      if (newCalc.result === 0) newCalc.result = parseFloat(mainString);
+      if (newCalc.result === 0) newCalc.result = parseFloat(mainStr);
       setPastEquations(
         pastEquations.concat(
           <div key={pastEquations.length} className="history">
-            <p>{mainString}</p>
+            <p>{mainStr}</p>
             <p>= {newCalc.result}</p>
           </div>
         )
       );
-      if (newCalc.result !== 0) mainString = newCalc.result.toString();
+
+      if (newCalc.result !== 0) mainStr = newCalc.result.toString();
       newCalc.result = 0;
+      newCalc.displayedString = mainStr;
       setCalc(newCalc);
       return;
     }
 
     if (input === "<-") {
-      mainString = mainString.slice(0, -1);
-      if (mainString === "") mainString = "0";
-      newCalc.displayedString = mainString;
+      mainStr = mainStr.slice(0, -1);
+      if (mainStr === "") mainStr = "0";
+      newCalc.displayedString = mainStr;
       getCalculation(newCalc);
       setCalc(newCalc);
       return;
@@ -51,37 +53,42 @@ const PadBtn: React.FC<Props> = ({
 
     if (input === ",") input = ".";
 
-    if (
-      !isNumber(mainString.charAt(mainString.length - 1)) &&
-      !isNumber(input)
-    ) {
-      mainString = mainString.slice(0, -1);
+    if (input === "±" && mainStr === "0") return;
+
+
+    if (!isNumber(mainStr.charAt(mainStr.length - 1)) && !isNumber(input)) {
+      mainStr = mainStr.slice(0, -1);
     }
 
-    if (mainString.slice(-1) === "0" && isNumber(input))
-      mainString = mainString.slice(0, -1); //clear first zero
+    if (mainStr.slice(-1) === "0" && isNumber(input))
+      mainStr = mainStr.slice(0, -1); //clear first zero
 
-    let isLessThan8 = newCalc.nums[newCalc.nums.length - 1].length < 8;
-    if (isLessThan8) mainString += input; //need improvement
+    let numberLenght = 0;
+    for (let i = 0; i < mainStr.length; i++) {
+      if (isNumber(mainStr.charAt(i))) numberLenght++;
+      else numberLenght = 0;
+    }
+
+    if (numberLenght < 8 || !isNumber(input)) mainStr += input;
 
     if (input === "±") {
-      //to fix: negative first input makes the equation be a subtraction
-      let toInvertNumber = newCalc.nums[newCalc.nums.length - 1];
-      mainString = mainString.slice(0, -1);
+      let numberToInvert = newCalc.nums[newCalc.nums.length - 1];
+      mainStr = mainStr.slice(0, -1);
       if (newCalc.sign[newCalc.sign.length - 1] === "-") {
-        mainString = mainString.replace(/-([^-]*)$/, "+" + "$1");
+        mainStr = mainStr.replace(/-([^-]*)$/, "+" + "$1");
         newCalc.sign[newCalc.sign.length - 1] = "+";
       } else if (newCalc.sign[newCalc.sign.length - 1] === "+") {
-        mainString = mainString.replace(/\+([^+]*)$/, "-" + "$1");
+        mainStr = mainStr.replace(/\+([^+]*)$/, "-" + "$1");
         newCalc.sign[newCalc.sign.length - 1] = "-";
       } else {
-        toInvertNumber = (parseFloat(toInvertNumber) * -1).toString();
-        mainString = mainString.slice(0, -toInvertNumber.length);
-        mainString = mainString.concat(toInvertNumber);
+        let invertedNumber = (parseFloat(numberToInvert) * -1).toString();
+        mainStr = mainStr.slice(0, -invertedNumber.length);
+        mainStr = mainStr.concat(invertedNumber);
       }
+      if (newCalc.nums.length <= 1) mainStr = "0" + mainStr;
     }
 
-    newCalc.displayedString = mainString;
+    newCalc.displayedString = mainStr;
     getCalculation(newCalc);
     console.log(newCalc);
     setCalc(newCalc);
@@ -96,28 +103,33 @@ const PadBtn: React.FC<Props> = ({
     newCalc.nums = [];
     newCalc.result = 0;
 
-    let mainString = newCalc.displayedString;
+    let mainStr = newCalc.displayedString;
 
-    newCalc.nums = mainString.split(/[+x÷\±-]/);
+    newCalc.nums = mainStr.split(/[+x÷±-]/);
     newCalc.nums = newCalc.nums.filter((str) => str !== "");
 
-    signsString = mainString.replace(/[0-9.]/g, "");
+    signsString = mainStr.replace(/[0-9.]/g, "");
     newCalc.sign = signsString.split("");
 
-    let doFirst = newCalc.sign.findIndex(
+    if (mainStr.charAt(0) === "-")
+      newCalc.result = parseFloat("-" + newCalc.nums[0]);
+    else newCalc.result = parseFloat(newCalc.nums[0]);
+
+    let signIndex = newCalc.sign.findIndex(
       (element) => element === "x" || element === "÷"
     );
-    equation(doFirst, newCalc);
+
+    handleMultiplicationsAndDivisions(signIndex, newCalc);
 
     newCalc.nums.forEach((element, index) => {
       if (newCalc.sign[index] === "%") {
-        mainString = mainString.slice(0, -element.length);
-        if (index === 0) element = makeItPercentage(1, element);
-        else element = makeItPercentage(newCalc.result, element);
-        mainString = mainString.concat(element);
+        mainStr = mainStr.slice(0, -element.length);
+        if (index === 0) element = trasformToPercentage(1, element);
+        else element = trasformToPercentage(newCalc.result, element);
+        mainStr = mainStr.concat(element);
       }
 
-      newCalc.displayedString = mainString;
+      newCalc.displayedString = mainStr;
 
       newCalc.result = getResult(
         newCalc.result,
@@ -128,7 +140,6 @@ const PadBtn: React.FC<Props> = ({
   };
 
   const getResult = (a: number, b: number, operation: string) => {
-    console.log(`a: ${a} b: ${b}`);
     switch (operation) {
       case "+":
         a += b;
@@ -142,34 +153,38 @@ const PadBtn: React.FC<Props> = ({
       case "÷":
         a /= b;
         break;
-      default: //undefined
-        a = b;
+      default:
         break;
     }
     return a;
   };
 
-  const equation = (doFirst: number, newCalc: Calc) => {
-    console.log(doFirst);
-    if (doFirst !== -1 && newCalc.nums[doFirst + 1]) {
-      newCalc.nums[doFirst + 1] = getResult(
-        parseFloat(newCalc.nums[doFirst]),
-        parseFloat(newCalc.nums[doFirst + 1]),
-        newCalc.sign[doFirst]
+  const handleMultiplicationsAndDivisions = (
+    signIndex: number,
+    newCalc: Calc
+  ) => {
+    let equationCalc = { ...newCalc };
+    if (signIndex !== -1 && equationCalc.nums[signIndex + 1]) {
+      equationCalc.nums[signIndex + 1] = getResult(
+        parseFloat(equationCalc.nums[signIndex]),
+        parseFloat(equationCalc.nums[signIndex + 1]),
+        newCalc.sign[signIndex]
       ).toString();
 
-      newCalc.nums.splice(doFirst, doFirst);
-      newCalc.sign.splice(doFirst, doFirst);
+      console.log(equationCalc.sign[signIndex]);
+      equationCalc.nums.splice(signIndex, 1);
+      equationCalc.sign.splice(signIndex, 1);
+      console.log(equationCalc.sign[signIndex]);
 
-      doFirst = newCalc.sign.findIndex(
+      signIndex = equationCalc.sign.findIndex(
         (element) => element === "x" || element === "÷"
       );
-      if (doFirst === -1) return;
-      equation(doFirst, newCalc);
+      if (signIndex === -1) return;
+      handleMultiplicationsAndDivisions(signIndex, equationCalc);
     }
   };
 
-  const makeItPercentage = (mainValue: number, percentageStr: string) => {
+  const trasformToPercentage = (mainValue: number, percentageStr: string) => {
     let percentageValue = parseFloat(percentageStr);
     percentageValue = (percentageValue / 100) * mainValue;
     percentageStr = percentageValue.toString();
